4a746f2ce7374b756d91ab6ebb4a966990681daa
Pahnal Aditia <fahnaladitia07@gmail.com>
2024-04-03 23:05:53 +0800

    Imported universal_html package and updated dependencies

--- lib/ui/components/side_section.dart
+++ lib/ui/components/side_section.dart
@@ -1,5 +1,5 @@
+import 'package:universal_html/html.dart' as html;
 import 'dart:math';
-import 'dart:html' as html;
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
